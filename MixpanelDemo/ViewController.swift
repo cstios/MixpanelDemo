b010b3a65352b9d90131e9c65720ef764b6bc133
Surabhi Chopada <surabhi.chopada@Administrators-Mac-mini.local>
2017-02-03 11:11:20 +0530

    Initial Commit

---
+++ MixpanelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MixpanelDemo
+//
+//  Created by Surabhi Chopada on 03/02/17.
+//  Copyright © 2017 Surabhi Chopada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
